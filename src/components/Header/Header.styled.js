ca3ca8408576f7656a48c1e1e060f3a0d948faf5
Mooteus <moteus10@gmail.com>
2022-05-02 10:53:21 -0300

    Refactor/Style: Header visual update
    header size and archive dropdow size

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -3,27 +3,34 @@ import styled from 'styled-components';
 export const Header = styled.header`
   background-color: #191919;
   width: 100%;
-  height: 50px;
+  height: 15px;
   display: flex;
-  flex-direction: column;
 `;
 
 export const UlOption = styled.ul`
+  margin: 0;
+  padding: 0;
   width: 100%;
   list-style-type: none;
 `;
 
 export const LiOption = styled.li`
+  margin-top: 1px;
+  margin-left: 1em;
+  height: 10px;
+  width: 30px;
   color: white;
-  width: 60px;
-  height: 30px;
+  font-size: 8px;
   cursor: pointer;
 `;
 
 export const ShowArchivesUl = styled.ul`
+  right: 10px;
+  display: flex;
+  flex-direction: column;
   padding: 0;
-  width: 150px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background-color: #191919;
   list-style: none;
   position: relative;
